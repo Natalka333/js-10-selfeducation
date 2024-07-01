12edfbb1cce10f621f5d78ddcee36df68d2e4e47
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-07-01 12:38:34 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+import Notiflix from 'notiflix';
+import { fetchBreeds } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
+
+
+const selectEl = document.querySelector('.breed-select');
+const loaderEl = document.querySelector('.loader');
+const catInfoEl = document.querySelector('.cat-info');
+
+
+function renderFeachBreeds() {
+    fetchBreeds().then(breeds => {
+        const optionEl = breeds.map(({ name, id }) => {
+            return `<option ${id}>${name}</option>`;
+        })
+        selectEl.insertAdjacentHTML('beforeend', optionEl);
+        new SlimSelect({
+            select: 'breed-select'
+        })
+    }).catch(error => {
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    }).finally(() => {
+
+    })
+}
 
 
