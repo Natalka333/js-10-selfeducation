79bbd9ad963e44886467bae54bd1460b0490164b
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-07-05 15:40:53 +0000

    Deploying to gh-pages from @ Natalka333/js-10-selfeducation@8a9f755dd6522a861e67c4f48c3100c57c6363c4 🚀

--- example.fc6691b8.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".btn"),n=document.querySelector(".user-list");e.addEventListener("click",(()=>{fetch("https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name").then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>function(e){const t=e.map((e=>`<li>\n        <p><b>Name</b>: ${e.name}</p>\n        <p><b>Email</b>: ${e.email}</p>\n        <p><b>Company</b>: ${e.company.name}</p>\n    </li>`)).join("");n.insertAdjacentHTML("beforeend",t)}(e))).catch((e=>console.log(e)))}));
-//# sourceMappingURL=example.fc6691b8.js.map
